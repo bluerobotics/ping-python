a60ee0cb111804b6e05e1040fe167041b58a8816
Nick Nothom <nicknothom@gmail.com>
2016-11-14 07:50:54 -0600

    Got simple demo working

--- Ping.py
+++ Ping.py
@@ -1,8 +1,16 @@
+#!/usr/bin/python -u
+
 import struct
 import serial
 
 #Open the serial port
 ser = serial.Serial('/dev/ttyUSB0', 115200)
+#ser = serial.Serial('/dev/tty.usbserial-A5059KLC', 115200)
+
+#Sonar report packet
+#452 Bytes
+packetFormat = "<ccHHhiiiiihhhhiIIhHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHcc"
+
 
 while True:
     buf = []
@@ -11,7 +19,7 @@ while True:
     try:
         #Burn through data until start signal
         while(ser.read() != "s"):
-            true
+            pass
         #Check second start signal
         if (ser.read() != "s"):
             break
@@ -19,33 +27,20 @@ while True:
         #Add start signal to buffer, since we have a valid message
         buf.append("s")
         buf.append("s")
-        data += struct.pack("B", 83)
-        data += struct.pack("B", 83)
+        data += struct.pack("<B", 83)
+        data += struct.pack("<B", 83)
 
         #Get the content of the message
-        #Message is 444 bytes long
-        for i in range(0,442):
+        for i in range(0,450):
             byte = ser.read()
-            data += struct.pack("c", byte)
+            data += struct.pack("<c", byte)
             buf.append(byte)
 
-        #This one works and is just bytes. 444 Bytes long.
-        #unpacked = struct.unpack("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",data)
-
-        #This one works with all of the metadata. But contains bytes for the data.
-        #There are 394 bytes for the profile data. Meaning 197 data points.
-        #unpacked=  struct.unpack("ccHHhiiiiihhhhiIIhBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBcc",data)
-
-        #This one is for experimentation.. I found that this one works, but it cuts off early. The end characters are not reached.
-        #For the depth profile data, it only has 197 values when I expect 200.
-        unpacked = struct.unpack("ccHHhiiiiihhhhiIIhHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH",data)
-
-        #This should technically be correct for everything, but doesn't work
-        #unpacked = struct.unpack("BBHHhiiiiihhhhiIIhHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHBB",data)
-
+        unpacked = struct.unpack(packetFormat, data)
 
         print(unpacked)
         print()
 
-    except:
+    except(Exception, e):
+        print("Error: "+str(e))
         pass
