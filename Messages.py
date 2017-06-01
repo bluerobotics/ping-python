7779f813f498453363743ca84d36d8990b62a95b
Nick Nothom <nicknothom@gmail.com>
2017-06-01 14:34:39 -0500

    Message class definition

--- Messages.py
+++ Messages.py
@@ -1,1 +1,10 @@
 #Messages.py
+
+class Messages:
+    def __init__(id, format, processor = None):
+        self.id = id
+        self.format = format
+        self.processor = processor
+
+
+#msg_gen_bootloader = (100, "")
