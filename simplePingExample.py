d2b187bd84ced380a52d171a71828e17d05b4b01
Nick Nothom <nicknothom@gmail.com>
2017-06-05 00:01:00 -0600

    implement list / array handling

--- simplePingExample.py
+++ simplePingExample.py
@@ -41,5 +41,5 @@ raw_input("Press Enter to continue...")
 
 # Read and print distance measurements with confidence
 while True:
-    myPing.getDistance()
+    myPing.getProfile()
     print("Distance: " + str(myPing.distance) + " Confidence: " + str(myPing.confidence))
