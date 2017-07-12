237cf3c7d2d495f795cb9058f7e25d59f04252d2
Jacob Walser <jwalser90@gmail.com>
2017-07-12 12:48:21 -0400

    Use argparse

--- simplePingExample.py
+++ simplePingExample.py
@@ -1,31 +1,18 @@
 #!/usr/bin/python -u
+
 #simplePingExample.py
 from Ping import Ping1D
 import sys
 import getopt
 import time
-
-device = ''
-instructions = "Usage: python simplePingExample.py -d <device_name>"
+import argparse
 
 ##Parse Command line options
 ############################
-try:
-    options, remainder = getopt.getopt(sys.argv[1:],"hd:",["help", "device="])
-except:
-    print(instructions)
-    exit(1)
 
-for opt, arg in options:
-    if opt in ('-h', '--help'):
-        print(instructions)
-        exit(1)
-    elif opt in ('-d', '--device'):
-        if (arg != ''):
-            device = arg
-    else:
-        print(instructions)
-        exit(1)
+parser = argparse.ArgumentParser(description="Driver for the Water Linked Underwater GPS system.")
+parser.add_argument('--device', action="store", required=True, type=str, help="Ping device port.")
+args = parser.parse_args()
 
 #Make a new Ping
 myPing = Ping1D(device)
