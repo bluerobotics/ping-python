7dd54b8361eb8bdb8a0006a06dbc8aea70ac610d
Patrick José Pereira <patrickelectric@gmail.com>
2017-12-26 23:08:43 -0200

    rawSurvey: Update sonar-logs file logic
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- rawSurvey.py
+++ rawSurvey.py
@@ -8,6 +8,9 @@ import getopt
 import time
 import csv
 
+import os
+from os.path import expanduser
+
 #2to3
 from builtins import bytes
 
@@ -43,7 +46,12 @@ for opt, arg in options:
         exit(1)
 
 if (file is ''):
-    file = "/home/pi/sonar-logs/raw-"+time.strftime("%Y-%m-%d-%H-%M-%S")+".ping_packets1"
+    path = "{0}/sonar-logs/".format(expanduser("~"))
+    filename = "raw-{0}.ping_packets1".format(time.strftime("%Y-%m-%d-%H-%M-%S"))
+    #Check if path exist and create it
+    if not os.path.exists(path):
+        os.makedirs(path)
+    file = path + filename
 
 fout = open(str(file),'wb')
 writer = csv.writer(fout,delimiter=',')
