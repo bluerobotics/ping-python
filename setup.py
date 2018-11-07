2e360e698b8f0faee88c3f083563085112757aa1
Willian Galvani <williangalvani@gmail.com>
2018-11-07 11:38:05 -0200

    setup.py: Add the README.md content as long_description

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(name='ping-python',
       version='0.0.1-dev',
       description='A python module for the Blue Robotics Ping1D echosounder',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Blue Robotics',
       url='https://www.bluerobotics.com',
       packages=find_packages(), install_requires=['pyserial']
