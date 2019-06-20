25d5fe1dffe9e0e27920d0d4f4f29ea41fa28957
Willian Galvani <williangalvani@gmail.com>
2019-06-20 17:47:35 +0100

    ping1d.py: Add a 10ms delay to while loop to free some cpu

--- brping/ping1d.py
+++ brping/ping1d.py
@@ -133,6 +133,7 @@ class Ping1D(object):
             if msg is not None:
                 if msg.message_id == message_id:
                     return msg
+            time.sleep(0.01)
         return None
 
     ##
