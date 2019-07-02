be75da249eb17ac4e37e7022dc79368cde99f633
Jacob Walser <jwalser90@gmail.com>
2019-07-02 13:32:25 -0400

    travis: update ci and deployment

---
+++ ci/travis-ci-script.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+source ci/ci-functions.sh
+
+echob "generating message api..."
+test pip install jinja2
+test generate/generate-python.py --output-dir=brping
+
+echob "testing message api..."
+test python brping/pingmessage.py
+
+echob "installing package..."
+test python setup.py install
+
+echob "update gh pages..."
+test pip install pyOpenSSL
+test ci/deploy-gh-pages.sh
+
+echob "deploying..."
+test ci/deploy.sh
--- test-ci.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-import OpenSSL.SSL
-
-print("openssl imported ok")
