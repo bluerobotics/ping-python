d603f0a1d4cd297a858f542d432144c70df1de65
Jacob Walser <jwalser90@gmail.com>
2019-08-27 13:36:24 -0400

    tools: use python3

--- tools/ping1d-simulation.py
+++ tools/ping1d-simulation.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # This script simulates a Blue Robotics Ping Echosounder device
 # A client may connect to the device simulation on local UDP port 6676
