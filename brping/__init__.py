a1d068dbfb0dcc81daf823ee9cea575311240a82
Jacob Walser <jwalser90@gmail.com>
2019-08-28 14:08:03 -0400

    brping: update __init__.py to export device classes

--- brping/__init__.py
+++ brping/__init__.py
@@ -1,4 +1,6 @@
 #'Ping python package'
 from brping.definitions import *
 from brping.pingmessage import *
+from brping.device import PingDevice
 from brping.ping1d import Ping1D
+from brping.ping360 import Ping360
