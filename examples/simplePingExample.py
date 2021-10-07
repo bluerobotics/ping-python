985e7222bba0035860a587a469c4dd5d611000ac
BlueRobotics-CI <support@bluerobotics.com>
2021-10-07 13:34:13 +0000

    update autogenerated files for eeea13c5c7d224fad7f5612ad954857c2ac080de

--- examples/simplePingExample.py
+++ examples/simplePingExample.py
@@ -11,22 +11,12 @@ from builtins import input
 ############################
 
 parser = argparse.ArgumentParser(description="Ping python library example.")
-parser.add_argument('--device', action="store", required=False, type=str, help="Ping device port. E.g: /dev/ttyUSB0")
-parser.add_argument('--baudrate', action="store", type=int, default=115200, help="Ping device baudrate. E.g: 115200")
-parser.add_argument('--udp', action="store", required=False, type=str, help="Ping UDP server. E.g: 192.168.2.2:9090")
+parser.add_argument('--device', action="store", required=True, type=str, help="Ping device port.")
+parser.add_argument('--baudrate', action="store", type=int, default=115200, help="Ping device baudrate.")
 args = parser.parse_args()
-if args.device is None and args.udp is None:
-    parser.print_help()
-    exit(1)
 
-# Make a new Ping
-myPing = Ping1D()
-if args.device is not None:
-    myPing.connect_serial(args.device, args.baudrate)
-elif args.udp is not None:
-    (host, port) = args.udp.split(':')
-    myPing.connect_udp(host, int(port))
-
+#Make a new Ping
+myPing = Ping1D(args.device, args.baudrate)
 if myPing.initialize() is False:
     print("Failed to initialize Ping!")
     exit(1)
